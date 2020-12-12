ee2eb5b255687eaa3f15178a447ddc6eb2fb3000
Salabim-mb <m.balas1504@gmail.com>
2020-12-12 12:27:50 +0100

    added base setup

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const path_list = {
+
+};
+
+export default [
+
+];
---
+++ src/root/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from "react";
+import Router from "root/router/Router";
+import {BrowserRouter} from "react-router-dom";
+import {UserProvider} from "context/UserContext";
+
+const App = () => {
+  return (
+      <UserProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </UserProvider>
+  );
+};
+
+export default App;
